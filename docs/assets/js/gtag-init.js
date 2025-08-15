906e8ba9abeb6a0fe00b94e0fcfd6c0ed2bc499a
antidmg <antidmg@proton.me>
2025-08-15 16:14:42 -0400

    test: use only MkDocs Material built-in analytics

--- docs/assets/js/gtag-init.js
+++
@@ -1,5 +0,0 @@
-// Google Analytics gtag initialization
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-EH352HTY05');
